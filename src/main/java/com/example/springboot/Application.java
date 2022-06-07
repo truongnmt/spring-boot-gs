c25dd52868d8050bc543a0f604897441bed286be
TruongNM <truongnmt.dev@gmail.com>
2022-06-07 12:07:40 +0900

    student class

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,10 +1,13 @@
 package com.example.springboot;
 
+import com.example.springboot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,16 @@ public class Application {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
 	}
 
 }
