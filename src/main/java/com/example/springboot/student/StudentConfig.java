f9d5770af0691951deb860c8b2bedba0c9eebd8f
TruongNM <truongnmt.dev@gmail.com>
2022-06-07 19:01:34 +0900

    Transient

--- src/main/java/com/example/springboot/student/StudentConfig.java
+++ src/main/java/com/example/springboot/student/StudentConfig.java
@@ -18,15 +18,13 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, JANUARY, 5),
-                    21
+                    LocalDate.of(2000, JANUARY, 5)
             );
 
             Student alex = new Student(
                     "Alex",
                     "alex@gmail.com",
-                    LocalDate.of(2004, MAY, 21),
-                    20
+                    LocalDate.of(2004, MAY, 21)
             );
 
             repository.saveAll(
