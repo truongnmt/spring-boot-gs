98795b49fcea53a60a9692cf937fb28edb0ce245
TruongNM <truongnmt.dev@gmail.com>
2022-06-08 15:54:11 +0900

    Throw error if user email taken

--- src/main/java/com/example/springboot/student/StudentService.java
+++ src/main/java/com/example/springboot/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -22,6 +23,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
